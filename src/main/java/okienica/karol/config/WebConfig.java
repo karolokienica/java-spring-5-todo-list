a44d71bb30348a32c5320f66b7d28da8b015fb15
karolokienica1 <karol.okienica1@gmail.com>
2020-05-01 15:16:35 +0200

    added model, view and controller

---
+++ src/main/java/okienica/karol/config/WebConfig.java
@@ -0,0 +1,28 @@
+package okienica.karol.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "okienica.karol")
+public class WebConfig {
+
+//    == constants ==
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    public static final String RESOLVER_SUFIX = ".jsp";
+
+//    == bean methods ==
+    @Bean
+    public ViewResolver viewResolver(){
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(RESOLVER_PREFIX);
+        viewResolver.setSuffix(RESOLVER_SUFIX);
+        return viewResolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <h1>Hello ${user}</h1>
+    <h2>${welcomeMessage}</h2>
+</body>
+</html>
